112e13e5bdab349dd1cfbc288820a05bd8900732
Shivaram Venkataraman <shivaram@eecs.berkeley.edu>
2013-02-05 12:00:55 -0800

    Fix bugs in set_ampcamp_ami

--- set_ampcamp_ami.py
+++ set_ampcamp_ami.py
@@ -16,11 +16,12 @@ def main():
     set_s3_kv("ampcamp-amis", "latest-strata", args[0])
 
 def set_s3_kv(bucket, key, value):
-  conn = connect_ec2()
+  conn = connect_s3()
   s3_bucket = conn.get_bucket(bucket) 
   s3_key = s3_bucket.get_key(key)
   old_value = s3_key.get_contents_as_string()
-  s3_key.set_content_as_string(value)
+  s3_key.set_contents_from_string(value)
+  new_value = s3_key.get_contents_as_string()
   print "Changed value of " + key + " from " + old_value + " to " + new_value
 
 if __name__ == "__main__":
