ef64ad6f7abc3ead0b5be0033fb9ca4ae122e251
Shivaram Venkataraman <shivaram@eecs.berkeley.edu>
2013-02-04 23:22:42 -0800

    Add helper script to update ami-id in S3

---
+++ set_ampcamp_ami.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import boto
+from optparse import OptionParser
+from boto import *
+
+def main():
+  parser = OptionParser(usage="set_ampcamp_ami.py <ami-id>",
+      add_help_option=True)
+  (opts, args) = parser.parse_args()
+  if len(args) != 1:
+    parser.print_help()
+    sys.exit(1)
+  else:
+    set_s3_kv("ampcamp-amis", "latest-strata", args[0])
+
+def set_s3_kv(bucket, key, value):
+  conn = connect_ec2()
+  s3_bucket = conn.get_bucket(bucket) 
+  s3_key = s3_bucket.get_key(key)
+  old_value = s3_key.get_contents_as_string()
+  s3_key.set_content_as_string(value)
+  print "Changed value of " + key + " from " + old_value + " to " + new_value
+
+if __name__ == "__main__":
+  main()
